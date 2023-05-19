538a309281ef479a71aaee0908fe3196a5ad3d73
Alejandro Bustamante <adbdc.u@gmail.com>
2023-05-19 17:56:58 -0400

    Actualizado a funcionar con bd de sql

--- server/main.js
+++ server/main.js
@@ -5,32 +5,11 @@ const cors = require('cors');
 const app = express();
 app.use(bodyParser.json());
 
+const port = 3000;
 app.use(cors({
   origin: '*'
 }));
 
-const port = 3000;
-
-
-app.get('/', (req, res) => {
-  res.send('API is working');
-})
-
-app.post('/driver',(req, res) => {
-  const body = req.body;
-  const query = `INSERT INTO user(firstName, lastName, email) VALUES ('${body.firstName}', '${body.lastName}','${body.email}');`;
-  connection.connect();
-  connection.query( query, (err, rows, fields) => {
-    if (err) throw err
-    console.log('1 record inserted');
-  })
-  connection.end();
-  res.send('1 record inserted');
-})
-
-app.listen(port, () => {
-  console.log(`Project sample is running on: ${port}`)
-})
 
 const connection = mysql.createConnection({
   host: 'localhost',
@@ -38,4 +17,56 @@ const connection = mysql.createConnection({
   user: 'root',
   password: 'toor',
   database: 'db1'
-})
+});
+
+connection.connect((err) => {
+  if (err) {
+    console.error('Error connecting to database: ', err);
+    return;
+  }
+  console.log('Connected to database');
+});
+
+app.use(express.urlencoded({ extended: true }));
+
+app.get('/', (req, res) => {
+  res.send('API is working');
+});
+
+app.use(express.json());
+
+app.post('/driver', (req, res) => {
+  const body = req.body;
+  const query = `INSERT INTO user(firstName, lastName, email) VALUES ('${body.firstName}', '${body.lastName}','${body.email}');`;
+
+  connection.query(query, (err, rows, fields) => {
+    if (err) {
+      console.error('Error inserting record: ', err);
+      res.status(500).send('Error inserting record');
+      return;
+    }
+    console.log('1 record inserted');
+    //res.send('1 record inserted');
+    res.json({ message: '1 record inserted' });
+  });
+});
+
+app.post('/usuario', (req, res) => {
+  const body = req.body;
+  const query = `INSERT INTO usuario(nombre, apellido, correo_electronico, hash_contrasenia) VALUES ('${body.firstName}', '${body.lastName}','${body.email}','${body.password}');`;
+
+  connection.query(query, (err, rows, fields) => {
+    if (err) {
+      console.error('Error inserting record: ', err);
+      res.status(500).send('Error inserting record');
+      return;
+    }
+    console.log('1 record inserted');
+    //res.send('1 record inserted');
+    res.json({ message: '1 record inserted' });
+  });
+});
+
+app.listen(port, () => {
+  console.log(`Project sample is running on: ${port}`);
+});
